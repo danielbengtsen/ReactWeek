81a48eaf367b0c43abe26e15797b23b22fc9edf9
danielbengtsen <cph-db145@cphbusiness.dk>
2020-10-28 12:16:46 +0100

    Day 2, exercise 1 done

--- Day2-Wednesday/day2_ex/src/App.js
+++ Day2-Wednesday/day2_ex/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
+
+const interval = 5;
+let initialValue = 0;
 
 function App() {
+  // Check for localStorage
+  if(localStorage.getItem("count") != null) 
+  {
+    initialValue = Number(localStorage.getItem("count"));
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter initialValue={initialValue} interval={interval} />
     </div>
   );
 }
---
+++ Day2-Wednesday/day2_ex/src/Counter.js
@@ -0,0 +1,30 @@
+import {useState, useEffect} from 'react';
+
+function Counter(props)
+{   
+    const [countValue, setCountValue] = useState(props.initialValue);
+
+    function incrementCountValue() {
+        setCountValue(countValue + props.interval);
+    }
+
+    function decrementCountValue() {
+        setCountValue(countValue - props.interval);
+    }
+
+    useEffect(
+        () => {
+            localStorage.setItem("count", countValue);
+        }, [countValue]
+    )
+
+    return (
+        <div>
+            <p>{countValue}</p>
+            <button onClick={incrementCountValue}>+</button>
+            <button onClick={decrementCountValue}>-</button>
+        </div>
+    );
+}
+ 
+export default Counter;
